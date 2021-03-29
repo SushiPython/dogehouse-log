cba8ba63a30ae27c2b629f0f36824300089b11c3
Sushi <repl@sushipython.us>
2021-03-29 15:59:54 +0000

    remove seconds

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     let data = await coll.find({}).toArray()
     var [times, users, servers] = [data.map(o => o['time']), data.map(o => o['users']), data.map(o => o['servers'])]
     times = times.map(function (val) {
-      return `${val.getUTCMonth()}/${val.getUTCDate()}/${val.getUTCFullYear()} ${val.getUTCHours()}:${val.getUTCMinutes()}:${val.getUTCSeconds()}`
+      return `${val.getUTCMonth()}/${val.getUTCDate()}/${val.getUTCFullYear()} ${val.getUTCHours()}:${(val.getUTCMinutes().toString().length === 2) ? val.getUTCMinutes() : '0'+val.getUTCMinutes().toString()}`
     })
     res.render('index.html', {'times': times,'users': users,'servers': servers})
   })
