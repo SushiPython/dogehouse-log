6ae35cd5157ce0a62f7f01677479d504f804fb80
Sushi <repl@sushipython.us>
2021-03-30 17:47:17 +0000

    added api, added dygraph

--- index.js
+++ index.js
@@ -14,15 +14,47 @@ nunjucks.configure('views', {
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+app.get('/old', (req, res) => {
   MongoClient.connect(process.env.url, async function(err, db) {
     let coll = db.db('dogehouse').collection('room-logger')
     let data = await coll.find({}).toArray()
-    var [times, users, servers] = [data.map(o => o['time']), data.map(o => o['users']), data.map(o => o['servers'])]
+    let [times, users, servers] = [data.map(o => o['time']), data.map(o => o['users']), data.map(o => o['servers'])]
     times = times.map(function (val) {
       return `${val.getUTCMonth()}/${val.getUTCDate()}/${val.getUTCFullYear()} ${val.getUTCHours()}:${(val.getUTCMinutes().toString().length === 2) ? val.getUTCMinutes() : '0'+val.getUTCMinutes().toString()}`
     })
     res.render('index.html', {'times': times,'users': users,'servers': servers})
+  })
+})
+
+app.get('/', (req, res) => {
+  res.render('new.html')
+})
+
+app.get('/api/json', (req, res) => {
+  MongoClient.connect(process.env.url, async function(err, db) {
+    let coll = db.db('dogehouse').collection('room-logger')
+    let data = await coll.find({}).toArray()
+    let [times, users, servers] = [data.map(o => o['time']), data.map(o => o['users']), data.map(o => o['servers'])]
+    times = times.map(function (val) {
+      return `${val.getUTCMonth()}/${val.getUTCDate()}/${val.getUTCFullYear()} ${val.getUTCHours()}:${(val.getUTCMinutes().toString().length === 2) ? val.getUTCMinutes() : '0'+val.getUTCMinutes().toString()}`
+    })
+    res.render('api.html', {'data': JSON.stringify({'times': times,'users': users,'servers': servers})})
+  })
+})
+app.get('/api/csv', (req, res) => {
+  MongoClient.connect(process.env.url, async function(err, db) {
+  let coll = db.db('dogehouse').collection('room-logger')
+  let data = await coll.find({}).toArray()
+  let [times, users, servers] = [data.map(o => o['time']), data.map(o => o['users']), data.map(o => o['servers'])]
+  times = times.map(function (val) {
+    return `${val.getUTCMonth()}/${val.getUTCDate()}/${val.getUTCFullYear()} ${val.getUTCHours()}:${(val.getUTCMinutes().toString().length === 2) ? val.getUTCMinutes() : '0'+val.getUTCMinutes().toString()}`
+  })
+  let mergedList = [...times, ...users, ...servers]
+  let csv = 'Data,Users,Servers\n'
+  for (i=0; i<times.length; i++) {
+    csv += `${times[i]},${users[i]},${servers[i]}\n`
+  }
+  res.render('api.html', {'data': csv})
   })
 })
 
